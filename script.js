8f1ab29bccfd80e8cb29396d156ab7dba5b17fbf
D3RADOX <dmafreelance@gmail.com>
2025-06-21 22:22:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+// Simple flicker pulse
+setInterval(() => {
+  const text = document.querySelector('.line3');
+  if (text) {
+    text.style.opacity = Math.random() > 0.96 ? '0.7' : '1';
+  }
+}, 200);
